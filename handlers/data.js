273a7371ee5b8e35bfd5767f86ec72895d34a5df
Christian Medinilla <cris.medinill@gmail.com>
2021-02-05 09:58:09 -0600

    fixup! Add data to API

--- handlers/data.js
+++ handlers/data.js
@@ -1,10 +1,14 @@
+const { DATA_DAYS, DATA_MONTHS, NET_REVENUE } = require('../handlers/data_files');
+
 const getHandler = (req, res) => {
   console.info(req);
   res.status(200).send({
     data: {
-      error: null,
-      message: '',
-      stats: {},
+      error: {},
+      message: 'ok',
+      dataDays: DATA_DAYS,
+      dataMonths: DATA_MONTHS,
+      dataRevenue: NET_REVENUE,
     }
   });
 };
